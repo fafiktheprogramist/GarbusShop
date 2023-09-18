ea5c44912650334e42e8b6a74babc7d12c045fff
glabojko <glabojko@gmail.com>
2023-09-18 18:34:37 +0200

    checkout model in progress part two

--- src/Codecool.CodecoolShop/Models/CheckoutModel.cs
+++ src/Codecool.CodecoolShop/Models/CheckoutModel.cs
@@ -1,24 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Codecool.CodecoolShop.Models
 {
     public class CheckoutModel : BaseModel
     {
+        [BindProperty]
+        [Required(ErrorMessage = "The first name is required")]
         public string FirstName { get; set; } = "";
-        public string LastName { get; set; } = "";
-        public string Email { get; set; } = "";
-        public string PhoneNumber { get; set; } = "";
-        public string CountryBill { get; set; } = "";
-        public string CityBill { get; set; } = "";
-        public string ZipCodeBill { get; set; } = "";
-        public string StreetBill { get; set; } = "";
-        public string NumberBill { get; set; } = "";
-        public string CountryShip { get; set; } = "";
-        public string CityShip { get; set; } = "";
-        public string ZipCodeShip { get; set; } = "";
-        public string StreetShip { get; set; } = "";
-        public string NumberShip { get; set; } = "";
+        [BindProperty] public string LastName { get; set; } = "";
+        [BindProperty] public string Email { get; set; } = "";
+        [BindProperty] public string PhoneNumber { get; set; } = "";
+        [BindProperty] public string CountryBill { get; set; } = "";
+        [BindProperty] public string CityBill { get; set; } = "";
+        [BindProperty] public string ZipCodeBill { get; set; } = "";
+        [BindProperty] public string StreetBill { get; set; } = "";
+        [BindProperty] public string NumberBill { get; set; } = "";
+        [BindProperty] public string CountryShip { get; set; } = "";
+        [BindProperty] public string CityShip { get; set; } = "";
+        [BindProperty] public string ZipCodeShip { get; set; } = "";
+        [BindProperty] public string StreetShip { get; set; } = "";
+        [BindProperty] public string NumberShip { get; set; } = "";
+
+        public string successMessage = "";
+        public string errorMessage = "";
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            if (!Codecool.CodecoolShop.Models.CheckoutModel.IsVali)
+            {
+                errorMessage = "Data validation failed";
+                return;
+            }
+            successMessage = "Your checkout form has been received correctly.";
+
+            FirstName = "";
+            LastName = "";
+            PhoneNumber = "";
+            CountryBill = "";
+            CityBill = "";
+            ZipCodeBill = "";
+            StreetBill = "";
+            NumberBill = "";
+            CountryShip = "";
+            CityShip = "";
+            ZipCodeShip = "";
+            StreetShip = "";
+            NumberShip = "";
+
+            ModelState.Clear();
+
+        }
 
     }
 }
