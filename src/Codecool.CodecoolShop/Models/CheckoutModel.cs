d1489f3c36d953df9b17af9c0d61ddf52a192e27
glabojko <glabojko@gmail.com>
2023-09-18 17:30:06 +0200

    checkout model in progress

---
+++ src/Codecool.CodecoolShop/Models/CheckoutModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class CheckoutModel : BaseModel
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public string CountryBill { get; set; } = "";
+        public string CityBill { get; set; } = "";
+        public string ZipCodeBill { get; set; } = "";
+        public string StreetBill { get; set; } = "";
+        public string NumberBill { get; set; } = "";
+        public string CountryShip { get; set; } = "";
+        public string CityShip { get; set; } = "";
+        public string ZipCodeShip { get; set; } = "";
+        public string StreetShip { get; set; } = "";
+        public string NumberShip { get; set; } = "";
+
+    }
+}
