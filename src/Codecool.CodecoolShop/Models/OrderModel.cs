9353242535de6c585a26dfe30d3180f61137cacb
fafiktheprogramist <glowaczra@gmail.com>
2023-09-20 00:28:08 +0200

    poprawione wyswietlanie w Order - wlasciwe dane sie pokazuja

--- src/Codecool.CodecoolShop/Models/OrderModel.cs
+++ src/Codecool.CodecoolShop/Models/OrderModel.cs
@@ -5,20 +5,20 @@ namespace Codecool.CodecoolShop.Models
     public class OrderModel
     {
         private int Id { get; set; }
-        public string FirstName { get; set; }
+        public string FirstName { get; private set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        private string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
         private string CountryBill { get; set; }
         private string CityBill { get; set; }
         private string ZipCodeBill { get; set; }
         private string StreetBill { get; set; }
         private string NumberBill { get; set; }
-        private string CountryShip { get; set; }
-        private string CityShip { get; set; }
-        private string ZipCodeShip { get; set; }
-        private string StreetShip { get; set; }
-        private string NumberShip { get; set; }
+        public string CountryShip { get; set; }
+        public string CityShip { get; set; }
+        public string ZipCodeShip { get; set; }
+        public string StreetShip { get; set; }
+        public string NumberShip { get; set; }
         public OrderModel(int id, string firstName, string lastName, string email, string phoneNumber,
             string countryBill, string cityBill, string zipCodeBill, string streetBill, string numberBill,
             string countryShip, string cityShip, string zipCodeShip, string streetShip, string numberShip)
